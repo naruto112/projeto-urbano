85e8bd0e135aab21df00f5c7f9b52217d9ee05bc
Renato Moschetta <renatorock3@hotmail.com>
2021-03-15 22:30:14 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TopoComponent, HomeComponent, RodapeComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Oferta } from './shared/oferta.model';
+
+@Injectable()
+export class OfertasService {
+  constructor(private http: HttpClient) {}
+
+  public getOfertas(): Promise<Oferta[]> {
+    return this.http
+      .get('http://localhost:3000/ofertas?destaque=true')
+      .toPromise()
+      .then((response: any) => response);
+  }
+}
